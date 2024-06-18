870d73618025c14020cd1276dba4c21046ecc40b
chosun-sangnom <kj82076@gmail.com>
2024-06-19 00:15:32 +0900

    레코드뷰 녹음기능이랑 버튼활성화 구현했습니다

---
+++ NC2_MachineLearning/NC2_MachineLearning/WaveformView.swift
@@ -0,0 +1,18 @@
+//
+//  WaveformView.swift
+//  NC2_MachineLearning
+//
+//  Created by 김준수(엘빈) on 6/18/24.
+//
+
+import SwiftUI
+
+struct WaveformView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WaveformView()
+}
