0f5481e15d2efa82a91806f4bdba4ca1c71e7bea
chosun-sangnom <kj82076@gmail.com>
2024-06-20 03:10:45 +0900

    commit

--- NC2_MachineLearning/NC2_MachineLearning/AppState.swift
+++ NC2_MachineLearning/NC2_MachineLearning/AppState.swift
@@ -5,4 +5,18 @@
 //  Created by 김준수(엘빈) on 6/20/24.
 //
 
-import Foundation
+
+
+
+import SwiftUI
+
+class AppState: ObservableObject {
+    @Published var isInitialScreen: Bool = true
+    @Published var isRecordViewActive: Bool = false
+    
+    func reset() {
+        isInitialScreen = true
+        isRecordViewActive = false
+    }
+}
+
--- NC2_MachineLearning/NC2_MachineLearning/Quotes.swift
+++ NC2_MachineLearning/NC2_MachineLearning/Quotes.swift
@@ -7,12 +7,20 @@
 
 import SwiftUI
 
-struct Quotes: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+struct QuoteData {
+    let quote: String
+    let movieTitle: String
 }
 
-#Preview {
-    Quotes()
+enum QuoteManager {
+    static let quotes: [QuoteData] = [
+        QuoteData(quote: "\" 잘못되고 있는 것에 집중하면 안 돼.\n항상 상황을 바꿀 방법이 있어. \"", movieTitle: "InsideOut, Joy"),
+        QuoteData(quote: "\" 다시 시도해. 다시 해보는 것을 두려워하지 마. \"", movieTitle: "The Matrix, Morpheus"),
+        QuoteData(quote: "\" 이 일은 전혀 제 기술을 테스트하지 않고 시작했어. \"", movieTitle: "Inception, Cobb"),
+        QuoteData(quote: "\" 당신이 할 수 있는 건 우리가 할 수 있는 최선을 다하는 것이다. 그게 전부야. \"", movieTitle: "Forrest Gump, Forrest Gump"),
+        QuoteData(quote: "\" 당신이 꿈꾸는 건 사실이 될 수 있어. 그래야만 한다. \"", movieTitle: "The Shawshank Redemption, Red")
+    ]
 }
+
+
+
